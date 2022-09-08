c3fce46a564868351114551745a9dca9ff1e89df
victor_ojedeji <ojedejivictordaniel@gmail.com>
2022-09-08 21:11:16 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+let main = document.querySelector(".main"),
+filterLogo = document.querySelector(".filter-img"),
+searchFilter =document.querySelector("#search"),
+elipsisBtn = document.querySelector(".elipsis");
+
+
+
+ filterLogo.addEventListener("click", show)
+ elipsisBtn.addEventListener("click", appear)
+let increment = 0;
+function show() {
+    increment = increment + 1;
+    if(increment === 3) {
+        increment = 1;
+    }
+    
+    if(increment === 1){
+        main.classList.add("show")
+    } else {
+        searchFilter.value = "";
+        main.classList.remove("show")
+    }
+};
+
+
+function appear() {
+    main.classList.add("appear")
+}
