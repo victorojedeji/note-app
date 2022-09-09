89dd4a475abff96a79e2f63fc02d3ff795f1b0a0
victor_ojedeji <ojedejivictordaniel@gmail.com>
2022-09-09 19:42:02 +0100

    styles added

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 let main = document.querySelector(".main"),
 filterLogo = document.querySelector(".filter-img"),
 searchFilter =document.querySelector("#search"),
-elipsisBtn = document.querySelectorAll(".elipsis");
+elipsisBtn = document.querySelectorAll(".elipsis"),
+add = document.querySelector(".add"),
+closeBtn = document.querySelector(".close-btn"),
+editBtn = document.querySelectorAll(".edit toggle");
 
-
-
- filterLogo.addEventListener("click", show)
+ filterLogo.addEventListener("click", show); 
+ add.addEventListener("click", popUp);
+ closeBtn.addEventListener("click", popUpClose);
+ 
+ 
+ 
+ editBtn.forEach( item => {
+     item.addEventListener("click", edit);
+});
  elipsisBtn.forEach(item => {
-    item.addEventListener("click", appear);
- })
+    item.addEventListener("click",appear);
+ }); 
+    
+    
 let increment = 0;
 function show() {
     increment = increment + 1;
@@ -24,8 +35,22 @@ function show() {
     }
 };
 
+function popUp() {
+    main.classList.add("popup");
+};
+
+function popUpClose() {
+    main.classList.remove("popup");
+}
 
 function appear() {
-    main.classList.add("appear")
-    // console.log(this.e.target)
+   event.target.classList.add("appear");
+   //console.log(event.target)
+   let parent = event.target.parentNode;
+   console.log(parent)
+   
+}
+
+function edit() {
+    main.classList.add("popup")
 }
