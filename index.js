5b7bcf70f87ab69ea5c4ca5b770903d32a3a8d26
victor_ojedeji <ojedejivictordaniel@gmail.com>
2022-09-10 16:44:33 +0100

    styles changed

--- index.js
+++ index.js
@@ -4,22 +4,34 @@ searchFilter =document.querySelector("#search"),
 elipsisBtn = document.querySelectorAll(".elipsis"),
 add = document.querySelector(".add"),
 closeBtn = document.querySelector(".close-btn"),
-editBtn = document.querySelectorAll(".edit toggle");
+editBtn = document.querySelectorAll(".edit toggle"),
+descriptionBox = document.querySelectorAll(".description"),
+addNote = document.querySelector(".add-btn"),
+titleTag = document.querySelector("#input-post"),
+descTag = document.querySelector("#text-area");
+
 
  filterLogo.addEventListener("click", show); 
  add.addEventListener("click", popUp);
  closeBtn.addEventListener("click", popUpClose);
- 
+ addNote.addEventListener("click", addNoteFunc)
  
  
  editBtn.forEach( item => {
-     item.addEventListener("click", edit);
+     item.addEventListener("click", edit)
 });
+
  elipsisBtn.forEach(item => {
-    item.addEventListener("click",appear);
+    item.addEventListener("click", appear);
  }); 
     
-    
+descriptionBox.forEach(item => {
+    item.addEventListener("click", expand)
+})   
+  
+const notes = JSON.parse(localStorage.getItem("notes") || "[]");     
+
+
 let increment = 0;
 function show() {
     increment = increment + 1;
@@ -44,13 +56,58 @@ function popUpClose() {
 }
 
 function appear() {
-   event.target.classList.add("appear");
-   //console.log(event.target)
    let parent = event.target.parentNode;
-   console.log(parent)
+   increment = increment + 1;
+   if(increment === 3) {
+        increment = 1;
+   }
    
+   if(increment === 1) {
+        parent.classList.add("appear")
+   } else {
+       parent.classList.remove("appear")
+   }
 }
 
+function expand() {
+    let parentEl = event.target.parentNode;
+    
+    increment = increment + 1;
+    if(increment=== 3) {
+        increment = 1;
+    }
+    
+    if(increment === 1) {
+        parentEl.classList.add("expand")
+    } else {
+        parentEl.classList.remove("expand")
+    }
+};
+
+function addNoteFunc(e) {
+    e.preventDefault();
+    let months = ["January", "February", " March", "April", " May", "June", " July", "August", " September", "October", "November", " December"];
+    
+    let titleNote = titleTag.value;
+    let descNote = descTag.value;
+    if(titleNote || descNote) {
+        let dateObj = new Date();
+        let date = dateObj.getDate();
+        let month = months[dateObj.getMonth()];
+        let year = dateObj.getFullYear();
+        let notes = [];
+        let noteObj = {
+            title: titleNote,
+            description: descNote,
+            date: `${month} ${date}, ${year}`
+        }
+        
+        notes.push(noteObj);
+        localStorage.setItem("notes", JSON.stringify(notes));
+    }
+}
+
+
 function edit() {
-    main.classList.add("popup")
+    main.classList.add("popup");
 }
